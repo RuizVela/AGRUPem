12691ed0cd94a8b1bb22bdeead05b29bc8534554
Raul <ruizvela271@gmail.com>
2019-11-29 09:36:16 +0100

    Factoria y Seeder de Eventos

---
+++ agrupem/database/factories/EventFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Event;
+use Faker\Generator as Faker;
+
+$factory->define(Event::class, function (Faker $faker) {
+    return [
+        'title'=>$faker->text(30),
+        'content_catalan'=>$faker->text(300),
+        'content_spanish'=>$faker->text(300)
+    ];
+});
