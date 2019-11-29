64a7adec095a5a5ba65710ed1b9c03a51a88ddb5
Raul <ruizvela271@gmail.com>
2019-11-29 09:01:27 +0100

    eventController todos los resources.

---
+++ agrupem/app/Http/Controllers/EventController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Event;
+use Illuminate\Http\Request;
+
+class EventController extends Controller
+{
+
+    public function index()
+    {
+        $events=Event::all();
+        return view('event.calendar',['events'=>$events]);
+    }
+
+    public function create()
+    {
+        return view('event.create');
+    }
+
+    public function store(Request $request)
+    {
+        Event::create($request->all());
+        return redirect('event');
+    }
+
+    public function show(Event $event)
+    {
+        return view('event.event',['event'=>$event]);
+    }
+
+    public function edit(Event $event)
+    {
+        return view('event.edit',['event'=>$event]);
+    }
+
+    public function update(Request $request, Event $event)
+    {
+        $event->update($request->all());
+        return redirect("event/$event->id");
+    }
+
+    public function destroy(Event $event)
+    {
+        $event->delete();
+        return redirect('event');
+    }
+}
