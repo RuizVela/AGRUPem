a6428e01bf0142b4ab4609fa27cc42872e8ba2f0
Raul <ruizvela271@gmail.com>
2019-11-29 12:00:50 +0100

    funcion index de Events devuelve los eventos ordenados por fecha

--- agrupem/app/Http/Controllers/EventController.php
+++ agrupem/app/Http/Controllers/EventController.php
@@ -10,7 +10,7 @@ class EventController extends Controller
 
     public function index()
     {
-        $events=Event::all();
+        $events=Event::sortByDate();
         return view('event.calendar',['events'=>$events]);
     }
 
