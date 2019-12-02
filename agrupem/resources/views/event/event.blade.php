cb92fc5ad5d07a59b859debb41c884d2db07cbed
Raul <ruizvela271@gmail.com>
2019-12-02 12:02:16 +0100

    Botones Editar y Eliminar evento en vista de evento

---
+++ agrupem/resources/views/event/event.blade.php
@@ -0,0 +1,13 @@
+Titulo: {{$event->title}}
+<table border="1px">
+<tr><td>Catalan <br>{{$event->content_catalan}}</td>
+    <td>Castellano <br>{{$event->content_spanish}}</td></tr>
+</table>
+<form action="/event/{{$event->id}}/edit" method="GET">
+    <input type="submit" value="Editar">
+</form>
+<form action="/event/{{$event->id}}" method="POST">
+    @csrf
+    @method('DELETE')
+    <input type="submit" value="Eliminar">
+</form>
