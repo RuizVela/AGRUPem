b5fb8009751b46650cb651c08bd1eea554370a0e
cotoflux <guaupelu@hotmail.com>
2019-12-02 17:03:15 +0100

    Creadas vistas, pendientes de terminar

---
+++ agrupem/resources/views/novelties/novelties.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <header class="card">
+                <div class="card-header">Novedades</div>
+            </header>
+            <main>
+            @foreach ($novelties as $novelty)
+            <section class="card">
+                <a id="resource_link" href="{{$novelty->url}}">{{$resource->title}}</a>
+
+                <form action="/resources/{{$resource->id}}/edit" method="GET">
+                    <input type="submit" id="button_edit" class = "btn btn-outline-primary mt-4" value="Editar">
+                </form>
+
+                <form action="/resources/{{$resource->id}}" method="post">
+                @csrf
+                @method('DELETE')
+                <input id="button_delete" type="submit" value="Eliminar" class="btn btn-outline-danger mt-4">
+                </form>
+
+            </section>
+            @endforeach
+            <a id="button_create" href="/resources/create" class="btn btn-outline-success mt-4">Crear</a>
+            </main>
+        </div>
+    </div>
+</div>
+
+@endsection
