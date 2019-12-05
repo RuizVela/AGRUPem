728e8b3c5203aff2ec75c11d6a485810ef4d3d3c
Raul <ruizvela271@gmail.com>
2019-12-05 13:04:14 +0100

    factoria user root

--- agrupem/database/seeds/DatabaseSeeder.php
+++ agrupem/database/seeds/DatabaseSeeder.php
@@ -7,6 +7,7 @@ class DatabaseSeeder extends Seeder
 
     public function run()
     {
+        $this->call(UserTableSeeder::class);
         $this->call(EventTableSeeder::class);
         $this->call(PostTableSeeder::class);
         $this->call(ResourceTableSeeder::class);
