31a63fe0de7875c589ecd3ca12991a11e4cf0569
kreighaistorm <jameskairlord@live.com>
2019-12-17 11:04:10 +0100

    changed dropdown menu to simple buttons

--- agrupem/storage/framework/views/fff0e40ceaaf97ca0139f86e6fd58148ceddb5c1.php
+++ agrupem/storage/framework/views/fff0e40ceaaf97ca0139f86e6fd58148ceddb5c1.php
@@ -49,11 +49,6 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
                             <?php $locale = session()->get('locale'); ?>
-                            <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        Idioma <span class="caret"></span>
-                                    </a>
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                         <form action="/lang/cat" method="GET">
                                         <input type="submit" value="Catalan"></form>
                                         <form action="/lang/es" method="GET">
