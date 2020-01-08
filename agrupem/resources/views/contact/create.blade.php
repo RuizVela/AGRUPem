4c50dc449e9bfe59592578aa08fab26533f30d10
cotoflux <guaupelu@hotmail.com>
2020-01-08 15:04:53 +0100

    Maquetación Contacto

--- agrupem/resources/views/contact/create.blade.php
+++ agrupem/resources/views/contact/create.blade.php
@@ -6,27 +6,24 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <main>
-                <section class="d-flex flex-row flex-nowrap align-items-center">
+                <section class="col-md-6 flex-row flex-nowrap align-items-center">
                     <form action="/contact" method="post">
                     @csrf
-                    <section class="flex-column p-2">
+                    <section class="col-md-12 flex-column p-1 window_information">
                         <div class="form-group">
-                            <label for="contact_name">@lang('contact.name')</label>
-                            <input id="contact_name" type="text" name="contact_name" value="{{old('contact_name')}}" class="form-control" required></input>
+                            <input id="contact_name" type="text" placeholder="@lang('contact.name')" name="contact_name" value="{{old('contact_name')}}" class="form-control" required></input>
                         </div>
                         <div class="form-group">
-                            <label for="contact_email">Email</label>
-                            <input id="contact_email" type="email" name="contact_email" value="{{old('contact_email')}}" class="form-control" required></input>
+                            <input id="contact_email" type="email" placeholder="Email"name="contact_email" value="{{old('contact_email')}}" class="form-control" required></input>
                         </div>
                         <div class="form-group">
-                            <label for="contact_message">@lang('contact.message')</label>
-                            <textarea id="contact_message" name="contact_message" cols="30" rows="10" class="form-control" required>{{old('contact_message')}}</textarea>
+                            <textarea id="contact_message" placeholder="@lang('contact.message')" name="contact_message" cols="30" rows="10" class="form-control" required>{{old('contact_message')}}</textarea>
                         </div>
                     </section>
                     <input type="submit" class="btn btn-outline-success mt-4" value="Enviar">
                     </form>
                     <section>
-                        <div id="map" style="width:400px; height:400px; margin-left:20px"></div>
+                        <div class="img-responsive" id="map" style="width:800px; height:400px; margin-left:20px"></div>
                     </section>
                 </section> 
             </main>
