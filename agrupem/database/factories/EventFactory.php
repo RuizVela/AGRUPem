37de4774d92c7a16e0b3e64963a384afbfdf21d5
Raul <ruizvela271@gmail.com>
2020-01-08 11:38:21 +0100

    factorias de eventos crean eventos mas adaptados a probar la aplicación

--- agrupem/database/factories/EventFactory.php
+++ agrupem/database/factories/EventFactory.php
@@ -4,13 +4,14 @@ use App\Event;
 use Faker\Generator as Faker;
 
 $factory->define(Event::class, function (Faker $faker) {
+    $startDate = $faker->dateTimeBetween('-10 days', '+30 days');
     return [
         'title_catalan'=>$faker->text(30),
         'title_spanish'=>$faker->text(30),
         'content_catalan'=>$faker->text(300),
         'content_spanish'=>$faker->text(300),
-        'startDate'=>$faker->date(),
-        'endDate'=>$faker->date(),
+        'startDate'=>$startDate,
+        'endDate'=>$faker->dateTimeBetween($startDate, $startDate->format('Y-m-d H:i:s').' +5 days'),
         'expired'=>false,
     ];
 });
