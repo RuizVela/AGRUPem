b558eaf31eba80e0a30c4ecf63203990c22eb4df
kreighaistorm <jameskairlord@live.com>
2020-01-09 12:31:11 +0100

    added box shadow and colors to home banner

--- agrupem/resources/views/_banner.blade.php
+++ agrupem/resources/views/_banner.blade.php
@@ -1,11 +1,10 @@
-<section id="banner_container" class="card left-container-card">
-    <div class="title card-header">
-        <h3>Què és l'esclerosi múltiple?</h3>
+<section id="banner_container">
+    <div class="home_content">
+        <p class="content">
+            L'esclerosi múltiple és una malaltia del sistema nerviós que pot provocar importants discapacitats
+            en les persones afectades i que, gairebé sempre, provoca grans canvis en la vida de l'afectat
+            i les persones que l'envolten.
+        </p>
     </div>
-    <p class="content">
-        L'esclerosi múltiple és una malaltia del sistema nerviós que pot provocar importants discapacitats
-        en les persones afectades i que, gairebé sempre, provoca grans canvis en la vida de l'afectat
-        i les persones que l'envolten.
-    </p>
 </section>
 <!-- TODO implementar cambio de lenguaje -->
