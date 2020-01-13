1e865a11d88ccf08ea248a41091bfbc6c0beca6b
angogi <andresmahanaim@gmail.com>
2020-01-13 20:02:53 +0000

    actualizacion angogi

--- agrupem/storage/framework/views/726112a1cd5e966e2be8f41e77a564d8affdae7d.php
+++ agrupem/storage/framework/views/726112a1cd5e966e2be8f41e77a564d8affdae7d.php
@@ -9,12 +9,11 @@
             <main>
             <?php $__currentLoopData = $novelties; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $novelty): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
             <section class="card">
+                <h3 id="resource_link" href="<?php echo e($novelty->url); ?>"><?php echo e($novelty->getLocalTitle()); ?></h3>
+                <p id="resource_link" href="<?php echo e($novelty->url); ?>"><?php echo Str::words($novelty->getLocalContent(), 20,"..."); ?></p>
                 <h3 id="title_catalan"><?php echo e($novelty->title_catalan); ?></h3>
                 <p id="content_catalan"><?php echo Str::words($novelty->content_catalan, 20,"..."); ?></p>
                 
-                <form action="/novelty/<?php echo e($novelty->id); ?>" method="GET">
-                    <input type="submit" id="button_show" class = "btn btn-outline-primary mt-4" value="Ver Mas">
-                </form>
                 <?php if(auth()->guard()->check()): ?>
                 <form action="/novelty/<?php echo e($novelty->id); ?>/edit" method="GET">
                     <input type="submit" id="button_edit" class = "btn btn-outline-primary mt-4" value="Editar">
