1ad60faf06ea91ee15ed50d107865b3ed4b0d41c
Raul <ruizvela271@gmail.com>
2020-01-14 11:32:06 +0100

    textos en ambos idiomas (ca/es) de la pagina colabora

--- agrupem/resources/views/colabora.blade.php
+++ agrupem/resources/views/colabora.blade.php
@@ -3,59 +3,24 @@
 <body>        
     <div id="colabora_title">
 
-            <h1>Colabora</h1>
+            <h1>@lang('colabora.colabora')</h1>
 
     </div>
     <div class="colabora_container">
     <div id="aportaciones_container">
-        <h1>Aportaciones</h1>
+        <h1>@lang('colabora.aportaciones')</h1>
         <div class="colabora_text">
-            <p>Si vols col.laborar econòmicament amb AGRUPem, des d'1 euro, entra a</p> 
-            <p>http://www.migranodearena.org/reto/18054/ajudans-a-fer-un-cim</p>
-
-            <p>o bé fés un ingrés al número de compte de "la Caixa":</p>
-            <p>2100 0295 18 0101642713</p>
+           @lang('colabora.aportaciones_text')
         </div>
     </div>
 
-        <h1>Voluntariados</h1>
+        <h1>@lang('colabora.voluntariados')</h1>
         <div class="colabora_text">
-            <p>Si vols col.laborar d'una altra manera posa't en contacte amb nosaltres a
-                 grupdesuport.em@gmail.com</p>
+            @lang('colabora.voluntariados_text')
         </div>
-        <h1 id="ec">Entidades Colaboradoras</h1>
+        <h1 id="ec">@lang('colabora.entidades_colaboradoras')</h1>
         <div class="colabora_text">
-            <p>Farmàcia Ros
-            <p>http://www.farmaciaortopediaros.com
-            <p>Farmàcia Guillermo
-            <p>Av. Catalunya, 4 (Castellbell i el Vilar)
-            
-            <p>FisioBages
-            <p>C/ Flor de lis, nº12 (Manresa)
-            
-            <p>Ortopèdia Orto7
-            <p> http://www.orto7.com
-            
-            <p> SIRN (Servei Integral de Rehabilitació Neurològica)
-            <p> http://www.sirnmanresa.cat
-            
-            <p>Casal cívic Mion-Puigberenguer
-            <p>Plaça Democràcia, nº1 (Manresa)
-            
-            <p>Piscines Municipals Manresa
-            <p>http://www.piscinesmanresa.cat
-            
-            <p>Ajuntament Manresa
-            <p>http://www.ajmanresa.cat
-            
-            <p>Clínica Universitària (CU+)
-            <p>http://www.clinicauniversitaria.cat
-            
-            <p>Fundació Althaia
-            <p>http://www.althaia.cat
-            
-            <p>Fundació Esclerosi Múltiple
-            <p>http://www.fem.es
+            @lang('colabora.entidades_colaboradoras_text')
         </div>
 </div>
     @endsection
