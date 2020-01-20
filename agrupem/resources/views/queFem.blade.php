93666dc359a8674e0ba54b808709fd2348e5cdcb
angogi <andresmahanaim@gmail.com>
2020-01-20 12:47:33 -1100

    queFem vista & mq finished

--- agrupem/resources/views/queFem.blade.php
+++ agrupem/resources/views/queFem.blade.php
@@ -1,21 +1,28 @@
 @extends('layouts.app')
 @section('content')
-<body>
-<div class="container general_container" >
-    <div class="">
-    <h1>@lang('layout.what-we-do')</h1> 
-        <div class="text_container">
-        <p>@lang('slider.what-we-do-content')</p>
+
+<section  class="container-fluid row section">
+    
+        <div class="shadow-lg p-3 mb-5 rounded col-sm-12 col-md-12 col-lg-6">
+            <div class="text_container text-center shadow-lg">
+            <h1 class="">@lang('layout.what-we-do')</h1>
+            </div>
+            <div class="text_container shadow-lg">
+            <p>@lang('slider.what-we-do-content')</p>
+            </div>
         </div>
-    </div>
-    <div class="right_content">
-    <div class="right_buttons">
-        <button type="button">Colabora</button>
-        <button type="button">Hazte Socio</button>
-    </div>
-        <img src="images/image1.jpg">
-        <img src="images/image2.jpg">
+        <div class="container right_content col-sm-12 col-md-12 col-lg-6">
+            <div class="d-flex flex-wrap flex-md-nowrap">
+                <button class="btn btn-primary btn-lg col-sm-12 col-md-6 col-lg-6 m-1" type="button">Colabora</button>
+                <button class="btn btn-primary btn-lg col-sm-12 col-md-6 col-lg-6 m-1" type="button">Hazte Socio</button>
+            </div>
+            <div class="">
+                <img class="col-sm-12 col-md-12 col-lg-12 " src="images/image1.jpg">
+                <img class="col-sm-12 col-md-12 col-lg-12 " src="images/image1.jpg">
+            </div>
         </div>
-</div>
-</body>
+    
+</section>
+
+
 @endsection
