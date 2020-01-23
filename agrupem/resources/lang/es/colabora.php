d4ab85219d3f8f4cb0a59f6300c8526202b9be2a
angogi <andresmahanaim@gmail.com>
2020-01-22 14:03:19 -1100

    camiar titulos y solucion enlaces

--- agrupem/resources/lang/es/colabora.php
+++ agrupem/resources/lang/es/colabora.php
@@ -3,8 +3,8 @@
 return [
     'colabora' => 'COLABORA',
     'aportaciones' => 'APORTACIONES',
-    'aportaciones_text' => '<p>Si quieres colaborar económicamente con AGRUPem, desde 1 euro, entra en
-    <p><a href src="http://www.migranodearena.org/reto/18054/ajudans-a-fer-un-cim">http://www.migranodearena.org/reto/18054/ajudans-a-fer-un-cim</a>
+    'aportaciones_text' => '<p>Si quieres colaborar económicamente con AGRUPem, desde 1 euro, entra 
+    <a href="http://www.migranodearena.org/reto/18054/ajudans-a-fer-un-cim">aquí</a>
 
     <p>o bien haz un ingreso en el número de cuenta de "la Caixa":
     <p>2100 0295 18 0101642713',
@@ -12,7 +12,7 @@ return [
     'voluntariados_text' => '<p>Si quieres colaborar de otra manera ponte en contacto con nosotros en grupdesuport.em@gmail.com',
     'entidades_colaboradoras' => 'ENTIDADES COLABORADORAS',
     'entidades_colaboradoras_text' => '<p>Farmàcia Ros<br>
-    <a href src="http://www.farmaciaortopediaros.com">http://www.farmaciaortopediaros.com</a>
+    <a href="http://www.farmaciaortopediaros.com">www.farmaciaortopediaros.com</a>
     
     <p>Farmacia Gruillermo <br>
     Av. Catalunya, 4 (Castellbell i el Vilar)
@@ -21,28 +21,28 @@ return [
     C/ Flor de lis, nº12 (Manresa)
     
     <p>Ortopèdia Orto7
-    <br> <a href src="http://www.orto7.com">http://www.orto7.com</a>
+    <br> <a href="http://www.orto7.com">www.orto7.com</a>
     
     <p>SIRN (Servei Integral de Rehabilitació Neurològica)
-    <br><a href src="http://www.sirnmanresa.cat">http://www.sirnmanresa.cat</a>
+    <br><a href="http://www.sirnmanresa.cat">www.sirnmanresa.cat</a>
     
     <p>Casal cívic Mion-Puigberenguer
     <br>Plaça Democràcia, nº1 (Manresa)
     
     <p>Piscines Municipals Manresa
-    <br><a href src="http://www.piscinesmanresa.cat">http://www.piscinesmanresa.cat</a>
+    <br><a href="http://www.piscinesmanresa.cat">www.piscinesmanresa.cat</a>
     
     <p>Ajuntament Manresa
-    <br><a href src="http://www.ajmanresa.cat">http://www.ajmanresa.cat</a>
+    <br><a href="http://www.ajmanresa.cat">www.ajmanresa.cat</a>
     
     <p>Clínica Universitaria (CU+)
-    <br><a href src="http://www.clinicauniversitaria.cat">http://www.clinicauniversitaria.cat</a>
+    <br><a href="http://www.clinicauniversitaria.cat">www.clinicauniversitaria.cat</a>
     
     <p>Fundación Althaia
-    <br><a href src="http://www.althaia.cat">http://www.althaia.cat</a>
+    <br><a href="http://www.althaia.cat">www.althaia.cat</a>
     
     <p>Fundación Esclerosis Múltiple
-    <br><a href src="http://www.fem.es">http://www.fem.es</a>'
+    <br><a href="http://www.fem.es">www.fem.es</a>'
 ];
 
 ?>
