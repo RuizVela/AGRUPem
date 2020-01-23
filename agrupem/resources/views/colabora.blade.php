1ad4ea3d553b53ae5f1f0f4452730e347049bb79
Raul <ruizvela271@gmail.com>
2020-01-23 15:47:07 +0100

    refactor: borrado unas clases sin nombre en colabora.blade

--- agrupem/resources/views/colabora.blade.php
+++ agrupem/resources/views/colabora.blade.php
@@ -8,21 +8,21 @@
     
         <div class="shadow-lg p-3 mb-5 rounded col-sm-12 col-md-12 col-lg-8"> 
             <div class="text_container text-center shadow-lg">
-                <h1 class="">@lang('colabora.aportaciones')</h1>
+                <h3>@lang('colabora.aportaciones')</h3>
             </div>
             <div class="text_container shadow-lg">
                 <p>@lang('colabora.aportaciones_text')</p>
             </div>
 
             <div class="text_container text-center shadow-lg">
-                <h1 class="">@lang('colabora.voluntariados')</h1>
+                <h3>@lang('colabora.voluntariados')</h3>
             </div>
             <div class="text_container shadow-lg">
                 <p>@lang('colabora.voluntariados_text')</p>
             </div>
 
             <div class="text_container text-center shadow-lg">
-                <h1 class="">@lang('colabora.entidades_colaboradoras')</h1>
+                <h3>@lang('colabora.entidades_colaboradoras')</h3>
             </div>
             <div class="text_container shadow-lg">
                 <p>@lang('colabora.entidades_colaboradoras_text')</p>
